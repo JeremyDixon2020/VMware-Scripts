ef6e89cc6fcc500f55101f9b1f143472d035bbb1
JeremyDixon2020 <59712798+JeremyDixon2020@users.noreply.github.com>
2020-01-09 17:33:35 -0600

    Add files via upload
    Script will scan all datastores in a cluster on each host and optimize each datastore presented from XtremIO to best practices. Script will disable SSH when done on all host in cluster.

---
+++ Host-Volume-Optimize-4-XIO.ps1
@@ -0,0 +1,31 @@
+﻿#written by Jeremy Dixon
+#Date:10-17-17
+
+
+Param(
+   [Parameter(Mandatory=$True,Position=1)]
+ [string]$vcenter,
+  [Parameter(Mandatory=$True,Position=2)]
+  [string]$ClusterName
+)
+
+
+
+Connect-VIServer -Server $vcenter
+
+
+foreach($esx in (Get-Cluster -Name $clusterName | Get-VMHost))
+{
+  $esxcli = Get-EsxCli -VMHost $esx
+  $devices = $esxcli.storage.core.device.list()
+  foreach ($device in $devices)
+  {
+    if ($device.Model -like “XtremApp”)
+    {
+      $esxcli.storage.core.device.set($false, $null, $device.Device, $null, $null, $null, $null, $null, $null, $null, $null, ‘256’,$null,$null)
+      $esxcli.storage.core.device.list()
+    }
+  }
+}
+
+Get-Cluster -Name $clusterName | Get-VMHost | ForEach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq “TSM-SSH”}) -Confirm:$FALSE}
